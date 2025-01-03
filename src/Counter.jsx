f7433424a5530acdcaf0806353b6e98c51dbb541
Sayan Saha <sayan199783@gmail.com>
2025-01-03 21:22:20 +0530

    Setup counter

---
+++ src/Counter.jsx
@@ -0,0 +1,18 @@
+
+const Counter = () => {
+   return(
+    <div className="col-12 col-md-4 offset-md-4 border text-white">
+        <span className="h2 pt-4 m-2 text-white-5">Fun Counter</span>
+        <br/>
+        <button className="btn btn-success m-1">+1</button> 
+        <button className="btn btn-danger m-1">-1</button>
+        <br/>
+        <span className="h4">
+            Conter : &nbsp;
+            <span className="text-success">0</span>
+        </span>
+    </div>
+   )
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './Header';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./Header";
+import Counter from "./Counter";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Header/>
+  <div>
+    <Header />
+    <div className="p-2 m-2 row text-center">
+      <Counter/>
+    </div>
+  </div>
 );
-
