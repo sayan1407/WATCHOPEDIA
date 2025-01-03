670769ba9548d7b69f8ee03c85c6ddd31c3d5bd4
Sayan Saha <sayan199783@gmail.com>
2025-01-03 21:54:29 +0530

    Another gotha with usestate

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,26 +2,30 @@ import { useState } from "react";
 const Counter = () => {
     const [counterState,setCounterState] = useState(() => {
         return {
-            counter : 10
+            counter : 10,
+            title : "Fun"
         }
     });
     const incrementCounter = () => {
         setCounterState((prevState) => {
             return {
-                counter : prevState.counter + 1
+                ...prevState,
+                counter : prevState.counter + 1,
+
             };
         })
     }
     const decrementCounter = () => {
         setCounterState((prevState) => {
             return {
+                ...prevState,
                 counter : prevState.counter - 1
             };
         })
     }
    return(
     <div className="col-12 col-md-4 offset-md-4 border text-white">
-        <span className="h2 pt-4 m-2 text-white-5">Fun Counter</span>
+        <span className="h2 pt-4 m-2 text-white-5">{counterState.title} Counter</span>
         <br/>
         <button className="btn btn-success m-1" onClick={incrementCounter}>+1</button> 
         <button className="btn btn-danger m-1" onClick={decrementCounter}>-1</button>
